6974855d2a91795932c26d868578fd2bebf2e470
Klaas Poortinga <poortinga.k@gmail.com>
2014-03-22 12:25:18 +0100

    Added default verbs, menu items now dynamic.

--- menu.js
+++ menu.js
@@ -4,32 +4,41 @@
 
 // Icon: vinkje! 
 
-//List of verbs
-var verbs = {
-	1: {
-			"verb": "read",
-			"url": "http://activitystrea.ms/schema/1.0/read"
-		},
-	2: {
-			"verb": "watched",
-			"url": "http://activitystrea.ms/schema/1.0/watch"
-		},
-	3: {
-			"verb": "completed",
-			"url": "http://activitystrea.ms/schema/1.0/complete"
-		},
-	4: {
-			"verb": "experienced",
-			"url": "http://activitystrea.ms/schema/1.0/experience"
-		},
-	5: {
-			"verb": "started",
-			"url": "http://activitystrea.ms/schema/1.0/start"
-		}
-};
+// Creating parent menu
+var parent = chrome.contextMenus.create({"title": "I did this:", contexts:["all"]});
+
+// Retrieving Verbs
+var verbsCollection;
+chrome.storage.sync.get("collection", function(obj){
+	verbsCollection = obj; // For later use.
+	for(var index in obj.collection){
+		chrome.contextMenus.create({"id": obj.collection[index]["verb"], "title": "I "+obj.collection[index]["verb"], "parentId": parent, contexts:["all"], "onclick": function(info){clicked(info.menuItemId, info.selectionText)}});
+	}
+});
+
+//Setting LRS vars
+var endpoint;
+var username;
+var password;
+var learner_name;
+var learner_email;
+// Retrieving LRS details
+chrome.storage.sync.get({
+		endpoint: 'https://cloud.scorm.com/ScormEngineInterface/TCAPI/public/',
+		username: 'LRS username',
+		password: 'LRS password',
+		learner_email: 'Email address',
+		learner_name: 'Your name'
+	}, function(items) {
+		endpoint = items.endpoint;
+		username = items.username;
+		password = items.password;
+		learner_name = items.learner_name;
+		learner_email = items.learner_email;
+	});
 
 // Adding menu items.
-var parent = chrome.contextMenus.create({"title": "I did this:", contexts:["all"]});
+
 //console.log(verbs);
 /*var menuitem = [];
 for(var index in verbs){
@@ -37,20 +46,76 @@ for(var index in verbs){
 }*/
 
 
-var child1 = chrome.contextMenus.create({"title": "I "+verbs[1]["verb"], "parentId": parent, contexts:["all"], "onclick": function(info){clicked(verbs[1]["verb"], info.selectionText, verbs[1]["url"])}});
-var child2 = chrome.contextMenus.create({"title": "I "+verbs[2]["verb"], "parentId": parent, contexts:["all"], "onclick": function(info){clicked(verbs[2]["verb"], info.selectionText, verbs[2]["url"])}});
-var child3 = chrome.contextMenus.create({"title": "I "+verbs[3]["verb"], "parentId": parent, contexts:["all"], "onclick": function(info){clicked(verbs[3]["verb"], info.selectionText, verbs[3]["url"])}});
-var child4 = chrome.contextMenus.create({"title": "I "+verbs[4]["verb"], "parentId": parent, contexts:["all"], "onclick": function(info){clicked(verbs[4]["verb"], info.selectionText, verbs[4]["url"])}});
-var child5 = chrome.contextMenus.create({"title": "I "+verbs[5]["verb"], "parentId": parent, contexts:["all"], "onclick": function(info){clicked(verbs[5]["verb"], info.selectionText, verbs[5]["url"])}});
+//var child1 = chrome.contextMenus.create({"id": verbs[1]["verb"], "title": "I "+verbs[1]["verb"], "parentId": parent, contexts:["all"], "onclick": function(info){console.log(info); clicked(verbs[1]["verb"], info.selectionText, verbs[1]["url"])}});
+//var child2 = chrome.contextMenus.create({"title": "I "+verbs[2]["verb"], "parentId": parent, contexts:["all"], "onclick": function(info){clicked(verbs[2]["verb"], info.selectionText, verbs[2]["url"])}});
+//var child3 = chrome.contextMenus.create({"title": "I "+verbs[3]["verb"], "parentId": parent, contexts:["all"], "onclick": function(info){clicked(verbs[3]["verb"], info.selectionText, verbs[3]["url"])}});
+//var child4 = chrome.contextMenus.create({"title": "I "+verbs[4]["verb"], "parentId": parent, contexts:["all"], "onclick": function(info){clicked(verbs[4]["verb"], info.selectionText, verbs[4]["url"])}});
+//var child5 = chrome.contextMenus.create({"title": "I "+verbs[5]["verb"], "parentId": parent, contexts:["all"], "onclick": function(info){clicked(verbs[5]["verb"], info.selectionText, verbs[5]["url"])}});
 
 // Adding a onclick event
-function clicked(verb, object, verbUrl) {
-  	console.log(verb);
-  	var verb = verb;
-	var selection = object;
-	var verbUrl = verbUrl;
+function clicked(verb, object) {
+	//Setting vars
+	var objectText;
+	var objecturl;
+	var verbUrl = verbsCollection.collection[verb]["url"]; //Retrieving verb url from verbsCollection.
+
+	chrome.tabs.query({ currentWindow: true, active: true}, function(tab){
+	  	if(object == undefined){
+			objectText = tab[0].title;
+			objectUrl = tab[0].url;
+		}else{
+			objectText = object;
+			objectUrl = tab[0].url;
+		}
+
+		// Debugging: a check if all the data is there.
+		console.log(endpoint);
+		console.log(username);
+		console.log(password);
+		console.log(learner_name);
+		console.log(learner_email);
+		console.log(verbUrl); 
+		console.log(verb);
+		console.log(objectUrl);
+		console.log(objectText);
+
+		//If all the above are set: (The above should probably be build in like a check)
+		// Prepare tin can class.
+		var tincan = new TinCan({
+			recordStores: [{
+				endpoint: endpoint,
+				username: username,
+				password: password
+			}]
+		});
+		
+		
+		
+		// Sending statement!
+		tincan.sendStatement({
+			"actor": {
+		        "name": learner_name,
+		        "mbox": learner_email
+		     },
+		     "verb": {
+		         "id": verbUrl,
+		         "display": {"en-US": verb}
+		         },
+		     "object": {
+				"id": objectUrl,
+		        "definition": {
+		            "name": { "en-US": objectText }
+		        }
+		    }
+		});
+	});
+	
+	
+
+	
+
   	// Query to the LRS and user info.
-	chrome.storage.sync.get({
+	/*chrome.storage.sync.get({
 		endpoint: 'https://cloud.scorm.com/ScormEngineInterface/TCAPI/public/',
 		username: 'LRS username',
 		password: 'LRS password',
@@ -76,13 +141,7 @@ function clicked(verb, object, verbUrl) {
 				}]
 			});
 			
-			// Debugging
-			// console.log(items.learner_name);
-			// console.log(items.learner_email);
-			// console.log(verbUrl); 
-			// console.log(verb);
-			// console.log(objectUrl);
-			// console.log(object);
+			
 			
 			// Sending statement!
 			tincan.sendStatement({
@@ -104,7 +163,7 @@ function clicked(verb, object, verbUrl) {
 
 	  	});
 	  	
-	});
+	});*/
 
   
   
